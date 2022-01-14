0a216abee18f5226d49de0e2ea41ba0da0194d13
aidanasingh <aidanxaxs@gmail.com>
2022-01-14 16:05:27 -0500

    Addressess issues with rendering Eigenmike B format files, makes according changes to _get_audio_numpy and its tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -37,9 +37,9 @@ def test_get_audio_numpy_valueerrors():
     with pytest.raises(ValueError):
         _get_audio_numpy("a", a.dataset, "B", "A")
 
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError):#This test needs replacement
         _get_audio_numpy(
-            ["a", "b", "c", "d"], a.dataset, "A", "B", {"a": [0, 0, 0]}, 1000
+            a.micarray_capsule_clip_ids["OCT3D"]["impulse_response+90d"], a.dataset, "A", "B", {"a": [0, 0, 0]}, 1000
         )
 
     with pytest.raises(ValueError):
@@ -68,17 +68,23 @@ def test_get_audio_numpy_resample():
         fs=24000,
     )
     wavs_dir = os.path.join(data_dir, "3D-MARCo Impulse Responses/01_Speaker_+90deg_3m")
-    wavs = os.listdir(wavs_dir)
+    wavs = [wavs_dir+"/+90deg_010_OCT3D_1_FL.wav",wavs_dir+"/+90deg_011_OCT3D_2_FR.wav",
+    wavs_dir+"/+90deg_012_OCT3D_3_FC.wav",wavs_dir+"/+90deg_013_OCT3D_4_RL.wav",
+    wavs_dir+"/+90deg_014_OCT3D_5_RR.wav",wavs_dir+"/+90deg_015_OCT3D_6_FLh_1m.wav",
+    wavs_dir+"/+90deg_016_OCT3D_7_FRh_1m.wav",wavs_dir+"/+90deg_017_OCT3D_8_RLh_1m.wav",
+    wavs_dir+"/+90deg_018_OCT3D_9_RRh_1m.wav"]
     wavs.sort()
     B = np.array(
-        [librosa.load(os.path.join(wavs_dir, w), 24000, mono=False)[0] for w in wavs]
+        [librosa.load(w, 24000, mono=False)[0] for w in wavs]
     )
     assert np.allclose(A, B, atol=1e-4)
 
 
 def test_get_audio_numpy_a2b():
     data_dir = "tests/resources/datasets/marco"
+
     a = marco(download=False, data_home=data_dir)
+
     A = _get_audio_numpy(
         a.micarray_capsule_clip_ids["OCT3D"]["impulse_response+90d"],
         a.dataset,
@@ -88,10 +94,41 @@ def test_get_audio_numpy_a2b():
         fs=48000,
     )
     wavs_dir = os.path.join(data_dir, "3D-MARCo Impulse Responses/01_Speaker_+90deg_3m")
-    wavs = os.listdir(wavs_dir)
+ 
+    wavs = [wavs_dir+"/+90deg_010_OCT3D_1_FL.wav",wavs_dir+"/+90deg_011_OCT3D_2_FR.wav",
+    wavs_dir+"/+90deg_012_OCT3D_3_FC.wav",wavs_dir+"/+90deg_013_OCT3D_4_RL.wav",
+    wavs_dir+"/+90deg_014_OCT3D_5_RR.wav",wavs_dir+"/+90deg_015_OCT3D_6_FLh_1m.wav",
+    wavs_dir+"/+90deg_016_OCT3D_7_FRh_1m.wav",wavs_dir+"/+90deg_017_OCT3D_8_RLh_1m.wav",
+    wavs_dir+"/+90deg_018_OCT3D_9_RRh_1m.wav"]
+
     wavs.sort()
+
     B = np.array(
-        [librosa.load(os.path.join(wavs_dir, w), sr=48000, mono=False)[0] for w in wavs]
+        [librosa.load(w, sr=48000, mono=False)[0] for w in wavs]
     )
+
     B = a2b(2, B, a.capsule_coords["OCT3D"])
+    assert np.allclose(A, B)
+
+    A = _get_audio_numpy(
+        a.micarray_capsule_clip_ids["Eigenmike"]["impulse_response+90d"],
+        a.dataset,
+        "A",
+        "B",
+        a.capsule_coords["Eigenmike"],
+        N=2,
+        fs=48000,   
+    )
+    
+    #wavs_dir = os.path.join(data_dir, "3D-MARCo Impulse Responses/01_Speaker_+90deg_3m")
+    #wavs = wavs_dir+"+90deg_065_Eigenmike_Raw_32ch.wav" 
+    #wavs.sort()
+
+    B = librosa.load(wavs_dir+"/+90deg_065_Eigenmike_Raw_32ch.wav", sr=48000, mono=False)[0]
+
+    B = a2b(2, B, a.capsule_coords["Eigenmike"])
+
+    print("Shape of A: ", A.shape)
+    print("Shape of B: ", B.shape)
+
     assert np.allclose(A, B)
