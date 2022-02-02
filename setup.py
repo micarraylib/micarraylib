006d3317749b801546fb9ea73e6aba9ffc3b6fd6
iranroman <iran@ccrma.stanford.edu>
2022-02-02 11:08:57 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup, find_packages
-setup(name='micarraylib', version='v0.1.1', packages=find_packages())
+setup(name='micarraylib', version='v0.1.2', packages=find_packages())
